a8dea8df8a92ae33e65e7ed2b05375d2f1c205b6
Lahiru-Randika <lahirurandika600@gmail.com>
2025-01-23 14:08:06 +0530

    made changes

--- frontend/vite.config.cjs
+++ frontend/vite.config.cjs
@@ -1,27 +1,27 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       "/api": {
-//           target: "http://localhost:8000",
-//           changeOrigin: true,
-//           rewrite: (path) => path.replace(/^\/api/, "/api"),
-//       },
-//     },
-//   },
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
   },
-});
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist', 
+//   },
+// });
